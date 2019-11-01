e82ae4161c133e26572c6520bcf48c0cd9e2f618
João Lopes <joaorfclopes@gmail.com>
2019-11-01 18:44:53 +0000

    Navbar added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,33 +2,60 @@ import React from "react";
 import logo from "../assets/svg/logo.svg";
 
 function scrollHome() {
-  document.getElementById("home").scrollIntoView();
+  const view = document.getElementById("home");
+  const homeButton = document.getElementById("homeButton");
+  const aboutButton = document.getElementById("aboutButton");
+  const contactsButton = document.getElementById("contactsButton");
+  view.scrollIntoView();
+  homeButton.classList.add("active");
+  aboutButton.classList.remove("active");
+  contactsButton.classList.remove("active");
 }
 function scrollAbout() {
-  document.getElementById("about").scrollIntoView();
+  const view = document.getElementById("about");
+  const homeButton = document.getElementById("homeButton");
+  const aboutButton = document.getElementById("aboutButton");
+  const contactsButton = document.getElementById("contactsButton");
+  view.scrollIntoView();
+  homeButton.classList.remove("active");
+  aboutButton.classList.add("active");
+  contactsButton.classList.remove("active");
 }
 function scrollContacts() {
-  document.getElementById("contacts").scrollIntoView();
+  const view = document.getElementById("contacts");
+  const homeButton = document.getElementById("homeButton");
+  const aboutButton = document.getElementById("aboutButton");
+  const contactsButton = document.getElementById("contactsButton");
+  view.scrollIntoView();
+  homeButton.classList.remove("active");
+  aboutButton.classList.remove("active");
+  contactsButton.classList.add("active");
 }
 
 export default function Navbar() {
   return (
     <div className="navbar">
       <div className="left">
-        <a href="#home">
+        <span onClick={scrollHome}>
           <img className="logo" src={logo} alt="logo" />
-        </a>
+        </span>
       </div>
       <div className="right">
         <ul className="links">
           <li className="link">
-            <span onClick={scrollHome}>HOME</span>
+            <span id="homeButton" onClick={scrollHome}>
+              HOME
+            </span>
           </li>
           <li className="link">
-            <span onClick={scrollAbout}>ABOUT</span>
+            <span id="aboutButton" onClick={scrollAbout}>
+              ABOUT
+            </span>
           </li>
           <li className="link">
-            <span onClick={scrollContacts}>CONTACTS</span>
+            <span id="contactsButton" onClick={scrollContacts}>
+              CONTACTS
+            </span>
           </li>
         </ul>
       </div>
