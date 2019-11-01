2cfafd19db0c2392e282d7710921ff587b84c50f
João Lopes <joaorfclopes@gmail.com>
2019-11-01 19:27:16 +0000

    Navbar for mobile missing

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 
 export default function About() {
-  return <div className="About view" id="about">
-    <h1>ABOUT</h1>
-  </div>;
+  return <div className="About view" id="about"></div>;
 }
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 
 export default function Home() {
-  return <div className="Home view" id="home">
-    <h1>HOME</h1>
-  </div>;
+  return <div className="Home view" id="home"></div>;
 }
