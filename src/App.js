a4db3eaa8ae8c8f7484455a8e51e1107af1db664
João Lopes <joaorfclopes@gmail.com>
2019-11-03 17:15:46 +0000

    Scroll to top on load added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React from "react";
 import Main from "./views/Main";
-import $ from "jquery";
 
 function App() {
-  $(document).ready(function() {
-    $(this).scrollTop(0);
-  });
   return (
     <div className="App">
       <Main />
--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./style.css";
+import $ from "jquery";
+
+$(document).ready(function() {
+  $(this).scrollTop(0);
+});
 
 ReactDOM.render(<App />, document.getElementById("root"));
