0a6a90c3a089007e0fa7d177316d12f57e9f555c
João Lopes <joaorfclopes@gmail.com>
2019-12-09 18:23:50 +0000

    Changed background and optimized images

--- src/components/AlertBrowser.jsx
+++ src/components/AlertBrowser.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Alert } from "react-bootstrap";
-import $ from "jquery";
 
 export default function AlertBrowser(props) {
   const [show, setShow] = React.useState(true);
