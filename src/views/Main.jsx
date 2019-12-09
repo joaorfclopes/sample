c1a7dd6508b3c5add7f1967a4cdc85f42e3fe65c
João Lopes <joaorfclopes@gmail.com>
2019-12-09 21:03:22 +0000

    Removed alert

--- src/views/Main.jsx
+++ src/views/Main.jsx
@@ -1,37 +1,16 @@
 import React from "react";
 import Navbar from "../components/Navbar";
-import Alert from "../components/AlertBrowser";
 import Home from "./Home";
 import About from "./About";
 import Contacts from "./Contacts";
-import { useMediaQuery } from "react-responsive";
 
 export default function Main() {
-  const isMobile = useMediaQuery({
-    query: "(max-device-width: 700px)"
-  });
-  const isDesktop = useMediaQuery({
-    query: "(min-device-width: 700px)"
-  });
   return (
     <div className="main">
-      {isMobile && (
-        <>
-          <Navbar />
-          <Home />
-          <About />
-          <Contacts />
-        </>
-      )}
-      {isDesktop && (
-        <>
-          <Navbar />
-          <Home />
-          <About />
-          <Contacts />
-          <Alert />
-        </>
-      )}
+      <Navbar />
+      <Home />
+      <About />
+      <Contacts />
     </div>
   );
 }
