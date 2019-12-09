046c11183a38e0b6de97b0bc30ad85a147f99ead
João Lopes <joaorfclopes@gmail.com>
2019-12-09 16:15:21 +0000

    Alert animation on close added

--- src/components/AlertBrowser.jsx
+++ src/components/AlertBrowser.jsx
@@ -1,24 +1,24 @@
 import React from "react";
 import { Alert } from "react-bootstrap";
+import $ from "jquery";
 
 export default function AlertBrowser(props) {
-  const [show, setShow] = React.useState(true);
-
-  if (show) {
-    return (
-      <Alert
-        className="alertBrowser"
-        id="alert"
-        variant="light"
-        onClose={() => setShow(false)}
-        dismissible
-      >
-        <p className="alertText">
-          For a better desktop experience it's recommended to use <u>Chrome</u>{" "}
-          or <u>Firefox</u>.
-        </p>
-      </Alert>
-    );
+  function closeAlert() {
+    $(".alertBrowser").addClass("fadeOut")
   }
-  return <div></div>;
+
+  return (
+    <Alert
+      className="alertBrowser"
+      id="alert"
+      variant="light"
+      onClose={() => closeAlert()}
+      dismissible
+    >
+      <p className="alertText">
+        For a better desktop experience it's recommended to use <u>Chrome</u> or{" "}
+        <u>Firefox</u>.
+      </p>
+    </Alert>
+  );
 }
